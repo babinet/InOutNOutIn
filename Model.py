7cdc9411a559f924c7bd44bb2b8f18df19c986cb
babinet <73350520+babinet@users.noreply.github.com>
2022-06-11 14:48:28 +0200

    Add files via upload

--- Model.py
+++ Model.py
@@ -1,4 +1,15 @@
+#!/usr/bin/python
 from osgeo import ogr
+from osgeo import osr
+source = osr.SpatialReference()
+source.ImportFromEPSG(3857)
+
+target = osr.SpatialReference()
+target.ImportFromEPSG(27561)
+
+transform = osr.CoordinateTransformation(source, target)
+
 wkt = "STRINGTOREPLACE"
 geom = ogr.CreateGeometryFromWkt(wkt)
-print "Length = %2f" % geom.Length()
+geom.Transform(transform)
+print ("Length = %d" % geom.Length())
